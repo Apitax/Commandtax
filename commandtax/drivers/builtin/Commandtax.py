657b60583de2bb1f2c314fbc7928e50ddab51a91
Shawn Clake <shawn.clake@gmail.com>
2018-10-21 16:49:17 -0600

    Upgraded dependencies

--- commandtax/drivers/builtin/Commandtax.py
+++ commandtax/drivers/builtin/Commandtax.py
@@ -33,40 +33,40 @@ class Commandtax(Driver):
 
         driverName = None
 
-        if (len(command.command) < 1):
+        if len(command.command) < 1:
             return response.res_bad_request(body={
                 'error': 'Improper command: `blank` command was attempted.'})
 
-        if ('--apitax-debug' in command.command):
+        if '--apitax-debug' in command.command:
             command.options.debug = True
             del command.command[command.command.index('--apitax-debug')]
 
-        if ('--apitax-sensitive' in command.command):
+        if '--apitax-sensitive' in command.command:
             command.options.sensitive = True
             del command.command[command.command.index('--apitax-sensitive')]
 
-        if ('--apitax-driver' in command.command):
+        if '--apitax-driver' in command.command:
             driverName = command.command[command.command.index('--apitax-driver') + 1]
             del command.command[command.command.index('--apitax-sensitive') + 1]
             del command.command[command.command.index('--apitax-driver')]
 
-        if (driverName):
+        if driverName:
             command.options.driver = LoadedDrivers.getDriver(driverName)
-        elif (LoadedDrivers.getDriver(command.command[0])):
+        elif LoadedDrivers.getDriver(command.command[0]):
             command.options.driver = LoadedDrivers.getDriver(command.command[0])
             del command.command[0]
         else:
             command.options.driver = LoadedDrivers.getPrimaryDriver()
 
-        if (not command.options.driver):
+        if not command.options.driver:
             return response.res_server_error(body={
                 'error': 'Could not find a valid driver to use for command: ' + str(command.command)})
 
         command.command = Command(command=command.command, request=command.request, parameters=command.parameters,
                                   options=command.options)
 
-        if (command.options.driver.isDriverCommandable()):
-            if (command.options.driver.isApiAuthenticated()):
+        if command.options.driver.isDriverCommandable():
+            if command.options.driver.isApiAuthenticated():
                 self.authenticate(command)
             response = command.options.driver.handleDriverCommand(command.command)
         else:
@@ -80,9 +80,9 @@ class Commandtax(Driver):
 
     # TODO: dont authenticate if credentials already exist for tokens
     def authenticate(self, command: Command):
-        if (command.options.driver.isApiAuthenticated()):
-            if (command.options.driver.isApiAuthenticationSeparateRequest()):
-                if (command.options.driver.isApiTokenable()):
+        if command.options.driver.isApiAuthenticated():
+            if command.options.driver.isApiAuthenticationSeparateRequest():
+                if command.options.driver.isApiTokenable():
                     command.credentials.token = command.options.driver.getApiToken(
                         command.options.driver.authenticateApi(command.credentials)).token
             else:
